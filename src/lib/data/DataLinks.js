5e63aa28764965ad68cb0d06ea64ea00a84ed53d
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-11-18 16:46:59 -0500

    Primera version y despliegue de la página

---
+++ src/lib/data/DataLinks.js
@@ -0,0 +1,6 @@
+export const linksApi = {
+  base: "https://api.themoviedb.org/3",
+  key: "?api_key=246e660d7ee8650b560e49acb51d798b",
+  keySearch: "&api_key=246e660d7ee8650b560e49acb51d798b",
+  images: "https://image.tmdb.org/t/p/w500",
+};
